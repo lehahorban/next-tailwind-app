a1ab968aafa6aac3ee42d18cce93640451d57558
Oleksii Horban <lehagorban93@gmail.com>
2023-05-16 14:26:36 +0300

    next.config.js. delete comments

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     domains: ["randomuser.me"],
-//   },
-// };
-
-// module.exports = nextConfig;
-
 module.exports = {
   reactStrictMode: true,
   images: {
